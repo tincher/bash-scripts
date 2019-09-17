699daae88ecb42925d1336a9102430eef95b7755
Joel Ewig <joel.ewig@gmail.com>
2019-09-17 17:45:34 +0200

    backups added

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,10 +1,19 @@
 #!/usr/local/bin/bash
-mapfile -t backupDirectories < backup.conf
 
-for backupDirectory in $backupDirectories
-do
-	cd $backupDirectory
-	git add .
-	git commit -m "$(date)"
-	git push
-done
+main(){
+	backupDirectories=()
+
+	mapfile -t backupDirectories < "./backup.conf"
+	for backupDirectory in ${backupDirectories[*]}; do
+		echo "$(date) $backupDirectory" >> /Users/joelewig/projects/bash-scripts/backup/log.log
+		cd $backupDirectory
+		git add .
+		git commit -m "$(date)"
+		git push
+
+	done
+
+
+}
+
+main
